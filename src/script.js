338fdc38707e4060b2d0b34fa2946180a7e61632
Shiyo Candy <claragalvs18@gmail.com>
2024-04-25 15:57:35 -0300

    mew commit

---
+++ src/script.js
@@ -0,0 +1,248 @@
+$(document).ready(function(){
+    $('#mobile-btn').on('click', function(){
+        $('#mobile-menu').toggleClass('active');
+        $('#mobile-btn').find('i').toggleClass('fa-x');
+    });
+
+    const sections = $('section');
+    const navItems = $('.nav-item');
+
+    $(window).on('scroll', function () {
+        const header = $('header');
+        const scrollPosition = $(window).scrollTop() - header.outerHeight();
+
+        let activeSectionIndex = 0;
+
+        if (scrollPosition <= 0) {
+            header.css('box-shadow', 'none');
+        } else {
+            header.css('box-shadow', '5px 1px 5px rgba(0, 0, 0, 0.1');
+        }
+
+        sections.each(function(i) {
+            const section = $(this);
+            const sectionTop = section.offset().top - 96;
+            const sectionBottom = sectionTop+ section.outerHeight();
+
+            if (scrollPosition >= sectionTop && scrollPosition < sectionBottom) {
+                activeSectionIndex = i;
+                return false;
+            }
+        })
+
+        navItems.removeClass('active');
+        $(navItems[activeSectionIndex]).addClass('active');
+    });
+
+});
+
+// notas 
+
+function popup() {
+
+    const popupContainer = document.createElement("div");
+
+    popupContainer.innerHTML = `
+    <div id="popupContainer">
+        <h1>Nova Nota</h1>
+        <textarea id="note-text" placeholder="Escreva sua nota..."></textarea>
+        <div id="btn-container">
+            <button id="submitBtn" onclick="createNote()">Criar nota</button>
+            <button id="closeBtn" onclick="closePopup()">Fechar</button>
+        </div>
+    </div>
+    `;
+    document.body.appendChild(popupContainer);
+}
+
+function closePopup() {
+    const popupContainer = document.getElementById("popupContainer");
+    if(popupContainer) {
+        popupContainer.remove();
+    }
+}
+
+function createNote() {
+
+    const popupContainer = document.getElementById('popupContainer');
+    const noteText = document.getElementById('note-text').value;
+    if (noteText.trim() !== '') {
+        const note = {
+        id: new Date().getTime(),
+        text: noteText
+        };
+
+        const existingNotes = JSON.parse(localStorage.getItem('notes')) || [];
+        existingNotes.push(note);
+
+        localStorage.setItem('notes', JSON.stringify(existingNotes));
+
+        document.getElementById('note-text').value = '';
+
+        popupContainer.remove();
+        displayNotes();
+    }
+}
+
+
+function displayNotes() {
+    const notesList = document.getElementById('notes-list');
+    notesList.innerHTML = '';
+
+    const notes = JSON.parse(localStorage.getItem('notes')) || [];
+
+    notes.forEach(note => {
+        const listItem = document.createElement('li');
+        listItem.innerHTML = `
+        <span>${note.text}</span>
+        <div id="noteBtns-container">
+            <button id="editBtn" onclick="editNote(${note.id})"><i class="fa-solid fa-pen"></i></button>
+            <button id="deleteBtn" onclick="deleteNote(${note.id})"><i class="fa-solid fa-trash"></i></button>
+        </div>
+        `;
+        notesList.appendChild(listItem);
+    });
+}
+function editNote(noteId) {
+    const notes = JSON.parse(localStorage.getItem('notes')) || [];
+    const noteToEdit = notes.find(note => note.id == noteId);
+    const noteText = noteToEdit ? noteToEdit.text : '';
+    const editingPopup = document.createElement("div");
+    
+    editingPopup.innerHTML = `
+    <div id="editing-container" data-note-id="${noteId}">
+        <h1>Edit Note</h1>
+        <textarea id="note-text">${noteText}</textarea>
+        <div id="btn-container">
+            <button id="submitBtn" onclick="updateNote()">Done</button>
+            <button id="closeBtn" onclick="closeEditPopup()">Cancel</button>
+        </div>
+    </div>
+    `;
+
+    document.body.appendChild(editingPopup);
+}
+
+function closeEditPopup() {
+    const editingPopup = document.getElementById("editing-container");
+
+    if(editingPopup) {
+        editingPopup.remove();
+    }
+}
+
+function updateNote() {
+    const noteText = document.getElementById('note-text').value.trim();
+    const editingPopup = document.getElementById('editing-container');
+
+    if (noteText !== '') {
+        const noteId = editingPopup.getAttribute('data-note-id');
+        let notes = JSON.parse(localStorage.getItem('notes')) || [];
+
+        
+        const updatedNotes = notes.map(note => {
+            if (note.id == noteId) {
+                return { id: note.id, text: noteText };
+            }
+            return note;
+        });
+
+       
+        localStorage.setItem('notes', JSON.stringify(updatedNotes));
+
+        editingPopup.remove();
+
+        
+        displayNotes();
+    }
+}
+
+
+function deleteNote(noteId) {
+    let notes = JSON.parse(localStorage.getItem('notes')) || [];
+    notes = notes.filter(note => note.id !== noteId);
+
+    localStorage.setItem('notes', JSON.stringify(notes));
+    displayNotes();
+}
+
+displayNotes();
+
+// calendario
+
+const daysTag = document.querySelector(".days"),
+currentDate = document.querySelector(".current-date"),
+prevNextIcon = document.querySelectorAll(".icons span");
+let date = new Date(),
+currYear = date.getFullYear(),
+currMonth = date.getMonth();
+const months = ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", 
+            "Setembro", "Outubro", "Novembro", "Dezembro"];
+const renderCalendar = () => {
+    let firstDayofMonth = new Date(currYear, currMonth, 1).getDay(), 
+    lastDateofMonth = new Date(currYear, currMonth + 1, 0).getDate(), 
+    lastDayofMonth = new Date(currYear, currMonth, lastDateofMonth).getDay(), 
+    lastDateofLastMonth = new Date(currYear, currMonth, 0).getDate(); 
+    let liTag = "";
+    for (let i = firstDayofMonth; i > 0; i--) { 
+        liTag += `<li class="inactive">${lastDateofLastMonth - i + 1}</li>`;
+    }
+    for (let i = 1; i <= lastDateofMonth; i++) { 
+        let isToday = i === date.getDate() && currMonth === new Date().getMonth() 
+                     && currYear === new Date().getFullYear() ? "active" : "";
+        liTag += `<li class="${isToday}">${i}</li>`;
+    }
+    for (let i = lastDayofMonth; i < 6; i++) { 
+        liTag += `<li class="inactive">${i - lastDayofMonth + 1}</li>`
+    }
+    currentDate.innerText = `${months[currMonth]} ${currYear}`; 
+    daysTag.innerHTML = liTag;
+}
+renderCalendar();
+prevNextIcon.forEach(icon => { 
+    icon.addEventListener("click", () => { 
+        currMonth = icon.id === "prev" ? currMonth - 1 : currMonth + 1;
+        if(currMonth < 0 || currMonth > 11) { 
+            date = new Date(currYear, currMonth, new Date().getDate());
+            currYear = date.getFullYear(); 
+            currMonth = date.getMonth(); 
+        } else {
+            date = new Date(); 
+        }
+        renderCalendar(); 
+    });
+});
+
+//modo change
+
+const $html = document.querySelector('html');
+const $checkbox = document.querySelector('#switch');
+
+$checkbox.addEventListener('change', function(){
+    $html.classList.toggle('light-mode')
+    
+});
+
+// let circle = document.querySelector('.colors');
+// circle.addEventListener("click", e =>{
+//     let target = e.target;
+//     if(target.classList.contains('circle')){
+//         circle.querySelector('.active').classList.remove('active');
+//         target.classList.add('active');
+//         document.querySelector('.imgBox .active').classList.remove('active');
+//         document.querySelector(` .imgBox .${target.id}`).classList.add('active');
+//     }
+// });
+
+document.getElementById('switch').addEventListener('click', function() {
+    var footerImage = document.getElementById('footer-image');
+    if (footerImage.classList.contains('dark')) {
+        footerImage.src = 'imgs/wave.png';
+        footerImage.classList.remove('dark');
+        footerImage.classList.add('light');
+    } else {
+        footerImage.src = 'imgs/wave (4).svg';
+        footerImage.classList.add('dark');
+        footerImage.classList.remove('light');
+    }
+});
